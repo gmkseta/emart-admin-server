ef3f42067a714efca4dad360d2be7b46793adf7e
gmkseta <gmkseta@gmail.com>
2019-05-27 19:47:03 +0900

    color

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,15 +1,3 @@
 //= require active_admin/base
 //= require active_admin_flat_skin
 
-
-$panelBorder: #babdc5;
-$skinActiveColor: #e73c3c;
-$bodyColor: rgba(236, 240, 241, 0.48);
-$skinBlack: rgb(55, 71, 79);
-$panelHeaderBck: $skinBlack;
-$skinBlackLight: rgb(69, 90, 100);
-$textGray: rgb(174, 179, 181);
-$skinTextColor: #1e2a33;
-$skinTextActiveColor: white;
-$skinTextTable: #7f8c8d;
-$skinHeaderBck: #1e2a33;
